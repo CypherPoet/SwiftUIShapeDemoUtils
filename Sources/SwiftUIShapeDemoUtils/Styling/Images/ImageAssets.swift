40c404c70a1fea458e3bd3e1e09732a286ca090a
CypherPoet <CypherPoet@gmail.com>
2021-03-23 06:14:05 -0500

    Move shared image assets to utils package.

---
+++ Sources/SwiftUIShapeDemoUtils/Styling/Images/ImageAssets.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+
+public enum ImageAssets {
+    public static let earthAtNight = Image("EarthAtNight", bundle: .module)
+    public static let nightSky1 = Image("NightSky1", bundle: .module)
+    public static let nightSky2 = Image("NightSky2", bundle: .module)
+    public static let mountain = Image("Mountain", bundle: .module)
+}
--- Sources/SwiftUIShapeDemoUtils/SwiftUIShapeDemoUtils.swift
+++
@@ -1,3 +0,0 @@
-struct SwiftUIShapeDemoUtils {
-    var text = "Hello, World!"
-}
